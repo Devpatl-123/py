d4fb6e787d77c8043beffb2f9e81187c26d7531f
Devpatl-123 <pateldev09866@gmail.com>
2025-01-24 13:44:03 +0530

    Create as1.py

---
+++ as1.py
@@ -0,0 +1,69 @@
+#1Arithmetic Operations
+#Write a program that:
+'''Declares two variables a = 10 and b = 5.
+Calculates and prints the result of the following operations:'''
+'''Addition (a + b)
+Subtraction (a - b)
+Multiplication (a * b)
+Division (a / b)
+Modulus (a % b)'''
+
+a = 10
+b = 5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+
+#2.Area of a Circle
+'''Write a program that:
+
+Declares a variable radius = 7.
+Calculates the area of a circle using the formula area = π * radius² (use 3.14 for π).
+Prints the area of the circle.'''
+
+r = 7
+π = 3.14
+print(π * r * r)
+
+#3. Comparison Operators
+'''Write a program that:
+
+Declares two variables x = 15 and y = 20.
+Checks and prints the result of the following comparisons:'''
+x = 15
+y = 20
+
+print(x > 20 and 15 < y) 
+print(15 < 20) 
+print(x==y)
+print(x!=y)
+print(x >= 10 and y <= 25)
+
+#4.Swap Two Variables
+'''Write a program to:
+
+Declare two variables a = 3 and b = 7.
+Swap their values without using a third variable.
+Print the new values of a and b.
+'''
+
+
+d = 3
+s = 7
+# d=s
+s=d
+print(d)
+
+
+#5. Simple Interest Calculation
+'''Write a program that:'''
+
+
+
+principal=5000
+rate =  5
+time = 2
+SI = (principal * rate * time) / 100
+print(SI)
